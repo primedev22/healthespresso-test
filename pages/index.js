59b69c0eb5065ad4d4dc871f6fbaf8e9b949eb3f
Prime Dev <primedev22@gmail.com>
2021-03-31 23:30:16 -0400

    Implement main Home page

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,104 @@
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Card from '../components/Card'
+import Toggle from '../components/Toggle'
+import Pagination from '../components/Pagination'
 
-export default function Home({ posts }) {
+const Container = styled.div`
+  min-height: 100vh;
+  padding: 0 0.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+const ToggleContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+`
+const Main = styled.main`
+  padding: 1em;
+  width: 100%;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+const Group = styled.div`
+  width: 100%;
+`
+const GroupLabel = styled.h2`
+  color: #FFFFFF;
+  background-color: #2196F3;
+  margin: 10px 0 0 0;
+  padding: 5px;
+`
+export default function Home({ posts, groupedPosts }) {
+  const rowsPerPage = 5
+  const total = posts.length
+  const [paginationMode, setPaginationMode] = useState(true)
+  const [groupMode, setGroupMode] = useState(false)
+  const [page, setPage] = useState(1)
+  const [paginatedPosts, setPaginatedPosts] = useState([])
+
+  useEffect(() => {
+    if (paginationMode) {
+      setPaginatedPosts(posts.slice(rowsPerPage * (page - 1), rowsPerPage* page))
+    }
+  }, [page])
+
+  const paginatedPostList = paginatedPosts.map(({id, userId, title, body}) => 
+    <Card key={id} userId={userId} title={title} description={body} />
+  )
+  const allPostList = posts.map(({id, userId, title, body}) => 
+    <Card key={id} userId={userId} title={title} description={body} />
+  )
+  const groupedPostList = Object.keys(groupedPosts).map(groupKey => 
+    <Group key={groupKey}>
+      <GroupLabel>User - {groupKey}</GroupLabel>
+      {groupedPosts[groupKey].map(({id, userId, title, body}) => 
+        <Card key={id} userId={userId} title={title} description={body} />
+      )}
+    </Group>
+  )
   return (
-    <div className={styles.container}>
+    <Container>
       <Head>
         <title>Coding Exercise</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Coding Exercise
-        </h1>        
-      </main>
+      <Main>
+        <ToggleContainer>
+          <Toggle label='Pagination mode' value={paginationMode} onChange={setPaginationMode} />
+          {!paginationMode && <Toggle label='Group mode' value={groupMode} onChange={setGroupMode} />}
+        </ToggleContainer>
 
-      <footer className={styles.footer}>
-        Written by HealthEspresso
-      </footer>
-    </div>
+        {paginationMode && paginatedPostList}
+        {!paginationMode && !groupMode && allPostList}
+        {!paginationMode && groupMode && groupedPostList}
+        {paginationMode && <Pagination page={page} rowsPerPage={5} total={total} onChange={setPage} />}
+      </Main>
+    </Container>
   )
 }
 
 Home.getInitialProps = async () => {
   try {
     const res = await fetch('https://jsonplaceholder.typicode.com/posts')
-    const json = await res.json()
-    return { posts: json }
+    const posts = await res.json()
+    const groupedPosts = {}
+    posts.forEach(post => {
+      if (groupedPosts[post.userId] === undefined) {
+        groupedPosts[post.userId] = [post]
+      } else {
+        groupedPosts[post.userId].push(post)
+      }
+    })
+    return { posts, groupedPosts }
   } catch (err) {
     return { posts: [] }
   }
